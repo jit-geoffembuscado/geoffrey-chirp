8a565e8984f776ea3e14d2512ab2e19963a85a77
arvinne <156976563+arvinne@users.noreply.github.com>
2024-06-06 15:17:16 +0800

    Added Chirp view navigation and Blade.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -11,10 +12,10 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() : Response
+    public function index() : View
     {
         //
-        return response('Hello, World!');
+        return view('chirps.index');
     }
 
     /**
